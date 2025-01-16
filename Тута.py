61e495c7345e5717fcb81a734d5c833b5a19294a
Fan-fi <naboychenko1201@gmail.com>
2025-01-16 18:18:02 +0300

    Add files via upload

---
+++ Тута.py
@@ -0,0 +1,46 @@
+import telebot 
+import random , os    
+# Замени 'TOKEN' на токен твоего бота
+# Этот токен ты получаешь от BotFather, чтобы бот мог работать
+bot = telebot.TeleBot("7801153685:AAGtb6z0OMsEmWDn0pzSAThVltqFrABEiuY")
+    
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот.Напиши /aco что бы узнать про мусор и /aco2 чтоб узнать как помочь планете")
+
+
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+    
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+
+@bot.message_handler(commands=['mem'])
+def send_mem(message):
+    images = os.listdir("images")
+    img_name = random.choice(images)
+    with open(f'images/{img_name}', 'rb') as f:  
+        bot.send_photo(message.chat.id, f)
+
+@bot.message_handler(commands=['aco'])
+def send_mem(message):
+    imagess = os.listdir("aco")
+    img_name = random.choice(imagess)
+    with open(f'aco/{img_name}', 'rb') as f:  
+        bot.send_photo(message.chat.id, f) 
+
+@bot.message_handler(commands=['aco2'])
+def send_mem(message):
+    image = os.listdir("aco2")
+    img_name = random.choice(image)
+    with open(f'aco2/{img_name}', 'rb') as f:  
+        bot.send_photo(message.chat.id, f) 
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+    
+bot.polling()
